312a68d79cd772af2e863db116f434a965acdc91
uta-jeh4448 <123279355+jeh4448@users.noreply.github.com>
2024-02-17 04:14:21 +0000

    added button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
-import React from 'react'
+import React from 'react';
+import Recipe from 'Recipe';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p>I am here.</p>
+        <button>
+      I don't do anything
+    </button>
       </div>
     </main>
   );
