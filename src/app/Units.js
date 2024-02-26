f3330db9006763bcfe349b608be043435b9080c4
uta-jeh4448 <123279355+jeh4448@users.noreply.github.com>
2024-02-26 17:33:10 +0000

    added in several conversions

--- src/app/Units.js
+++ src/app/Units.js
@@ -4,4 +4,99 @@ module.exports =  class Units
     {
         return amount/3;
     }
+
+    static TspFromTbsp(amount)
+    {
+        return amount * 3;
+    }
+
+    static cupFromTbsp(amount)
+    {
+        return amount/16;
+    }
+
+    static tbspnFromCup(amount)
+    {
+        return amount * 16;
+    }
+
+    static cupFromTsp(amount)
+    {
+        return amount/48;
+    }
+
+    static tspFromCup(amount)
+    {
+        return amount * 48;
+    }
+
+    static tbspFromCup(amount)
+    {
+        return amount/16;
+    }
+
+    static cupFromTbsp(amount)
+    {
+        return amount * 16;
+    }
+
+    static flozFromCup(amount)
+    {
+        return amount/8;
+    }
+    
+    static cupFromFloz(amount)
+    {
+        return amount * 8;
+    }
+
+    static pintFromCup(amount)
+    {
+        return amount *2;
+    }
+
+    static cupFromPint(amount)
+    {
+        return amount/2;
+    }
+
+    static quartFromPint(amount)
+    {
+        return amount/2;
+    }
+
+    static pintFromQuart(amount)
+    {
+        return amount * 2;
+    }
+
+    static cupFromQuart(amount)
+    {
+        return amount/4;
+    }
+
+    static quartFromCup(amount)
+    {
+        return amount * 4;
+    }
+
+    static gallonFromQuart(amount)
+    {
+        return amount/4;
+    }
+
+    static quartFromGallon(amount)
+    {
+        return amount * 4;
+    }
+
+    static poundFromOz(amount)
+    {
+        return amount/16;
+    }
+
+    static ozFromPound(amount)
+    {
+        return amount * 16;
+    }
 }
